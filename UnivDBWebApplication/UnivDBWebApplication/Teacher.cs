d570ee290bdb9f7807a8d92b7b745de9e69f2481
TheScientist-1 <108238495+TheScientist-1@users.noreply.github.com>
2023-05-27 16:31:55 +0300

    .

---
+++ UnivDBWebApplication/UnivDBWebApplication/Teacher.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UnivDBWebApplication;
+
+public partial class Teacher
+{
+    public int Id { get; set; }
+
+    public string LastName { get; set; } = null!;
+
+    public string FirstName { get; set; } = null!;
+
+    public DateTime HireDate { get; set; }
+
+    public int DepartmentId { get; set; }
+
+    public virtual Department Department { get; set; } = null!;
+}
